c4d9706f473e662ecc1975ab10ff833c46933058
Mike Alves <michelalvs@gmail.com>
2024-02-01 22:37:36 -0300

    Completed Lecture 312 - Implemented Payment Entity w One to One Association

--- src/main/java/com/mikxingu/curso_spring/config/TestConfig.java
+++ src/main/java/com/mikxingu/curso_spring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.mikxingu.curso_spring.entities.Category;
 import com.mikxingu.curso_spring.entities.Order;
 import com.mikxingu.curso_spring.entities.OrderItem;
+import com.mikxingu.curso_spring.entities.Payment;
 import com.mikxingu.curso_spring.entities.Product;
 import com.mikxingu.curso_spring.entities.User;
 import com.mikxingu.curso_spring.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2024-01-01T12:00:10Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }

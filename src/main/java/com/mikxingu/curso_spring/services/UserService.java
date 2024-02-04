0ddb7cb202d6933aa117a7613f070b871087f8db
Mike Alves <michelalvs@gmail.com>
2024-02-03 22:17:55 -0300

    Completed Lecture 316 - Implemented User Update Operation

--- src/main/java/com/mikxingu/curso_spring/services/UserService.java
+++ src/main/java/com/mikxingu/curso_spring/services/UserService.java
@@ -31,4 +31,20 @@ public class UserService {
 	public void deleteUser(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public User updateUser(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		
+		updateData(entity, obj);
+		
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+		
+	}
 }

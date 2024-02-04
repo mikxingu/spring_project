d73d87c39fee52905a983e5089c7e3ef3b896111
Mike Alves <michelalvs@gmail.com>
2024-02-03 22:41:05 -0300

    Completed Lecture 318 - Implemented Custom Exception handling for Delete User

--- src/main/java/com/mikxingu/curso_spring/services/UserService.java
+++ src/main/java/com/mikxingu/curso_spring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mikxingu.curso_spring.entities.User;
 import com.mikxingu.curso_spring.repositories.UserRepository;
+import com.mikxingu.curso_spring.services.exceptions.DatabaseException;
 import com.mikxingu.curso_spring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void deleteUser(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User updateUser(Long id, User obj) {

2e5c7435a79f51e1a1771c4f449fbba7adf5c33f
Mike Alves <michelalvs@gmail.com>
2024-02-03 22:44:07 -0300

    Completed Lecture 319 - Implemented Custom Exception handling for User updateg

--- src/main/java/com/mikxingu/curso_spring/services/UserService.java
+++ src/main/java/com/mikxingu/curso_spring/services/UserService.java
@@ -13,50 +13,57 @@ import com.mikxingu.curso_spring.repositories.UserRepository;
 import com.mikxingu.curso_spring.services.exceptions.DatabaseException;
 import com.mikxingu.curso_spring.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insertUser(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void deleteUser(Long id) {
 		try {
 			userRepository.deleteById(id);
-			
+
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
+
 	}
-	
+
 	public User updateUser(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		
-		updateData(entity, obj);
-		
-		return userRepository.save(entity);
+
+		try {
+			User entity = userRepository.getReferenceById(id);
+
+			updateData(entity, obj);
+
+			return userRepository.save(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
-		
+
 	}
 }

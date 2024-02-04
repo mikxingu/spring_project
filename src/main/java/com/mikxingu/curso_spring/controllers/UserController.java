a5af59743456f82951a066894d68b2272cf38f35
Mike Alves <michelalvs@gmail.com>
2024-02-03 22:07:13 -0300

    Completed Lecture 314 - Implemented User Insert Operation

--- src/main/java/com/mikxingu/curso_spring/controllers/UserController.java
+++ src/main/java/com/mikxingu/curso_spring/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.mikxingu.curso_spring.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mikxingu.curso_spring.entities.User;
 import com.mikxingu.curso_spring.services.UserService;
@@ -35,6 +39,18 @@ public class UserController {
 		return ResponseEntity
 				.ok()
 				.body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insertUser(@RequestBody User obj){
+		userService.insertUser(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity
+				.created(uri)
+				.body(obj);
+		
 	}
 	
 
